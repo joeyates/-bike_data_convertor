abb712820822f892c7b2fb539000dddf2afe9c3a
Joe Yates <joe.g.yates@gmail.com>
2021-10-24 16:44:38 +0200

    Remove generated code

--- lib/bike_data_convertor.ex
+++ lib/bike_data_convertor.ex
@@ -2,17 +2,4 @@ defmodule BikeDataConvertor do
   @moduledoc """
   Documentation for `BikeDataConvertor`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> BikeDataConvertor.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
