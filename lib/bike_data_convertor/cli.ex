5ea8801b6dfb19705fccf86aeeca6dafaa92c5d1
Joe Yates <joe.g.yates@gmail.com>
2023-10-25 21:17:50 +0200

    Ensure all logging is output

--- lib/bike_data_convertor/cli.ex
+++ lib/bike_data_convertor/cli.ex
@@ -1,9 +1,13 @@
 defmodule BikeDataConvertor.CLI do
   use Bakeware.Script
 
+  require Logger
+
   @impl Bakeware.Script
   def main(args) do
     Application.ensure_all_started(:logger)
-    BikeDataConvertor.Fit2Gpx.CLI.run(args)
+    result = BikeDataConvertor.Fit2Gpx.CLI.run(args)
+    Logger.flush()
+    result
   end
 end
